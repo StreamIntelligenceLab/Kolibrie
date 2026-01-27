11770470d76d26d45409e5961c9b04fa8f8288db
ladroid <puatachok@gmail.com>
2026-01-27 19:30:19 +0100

    Add header

--- kolibrie/src/disk_storage/mem_table.rs
+++ kolibrie/src/disk_storage/mem_table.rs
@@ -1,3 +1,13 @@
+/*
+ * Copyright © 2024 Volodymyr Kadzhaia
+ * Copyright © 2024 Pieter Bonte
+ * KU Leuven — Stream Intelligence Lab, Belgium
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v.2.0.If a copy of the MPL was not distributed with this file,
+ * you can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
 use shared::triple::Triple;
 use std::collections::BTreeMap;
 
