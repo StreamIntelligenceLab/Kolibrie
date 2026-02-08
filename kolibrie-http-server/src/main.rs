835254d58f867abcfc67ca5542ea87507a5b3dcd
ladroid <puatachok@gmail.com>
2026-02-08 16:20:15 +0100

    Add sidebar

--- kolibrie-http-server/src/main.rs
+++ kolibrie-http-server/src/main.rs
@@ -1,6 +1,11 @@
 /*
- * Copyright © 2026 Stream Intelligence Lab
+ * Copyright © 2026 Volodymyr Kadzhaia
+ * Copyright © 2026 Pieter Bonte
  * KU Leuven — Stream Intelligence Lab, Belgium
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * you can obtain one at https://mozilla.org/MPL/2.0/.
  */
 
 use std::io::{Read, Write};
