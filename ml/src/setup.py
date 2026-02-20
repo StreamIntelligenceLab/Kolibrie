7d3d7e0d0e9af194f17c6751a444ce03b579159d
ladroid <puatachok@gmail.com>
2026-02-20 17:32:03 +0100

    Update to official version 0.1.1

--- ml/src/setup.py
+++ ml/src/setup.py
@@ -12,7 +12,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="mlschema",
-    version="0.1.0",
+    version="0.1.1",
     py_modules=["mlschema"],
     packages=find_packages(),
     install_requires=[
